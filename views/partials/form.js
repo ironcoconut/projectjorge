0bc6bf922396995e18ccb4d5c4e8dcbe9d2c2fa0
Benjamin Ochoa <ben@law451.com>
2015-11-07 21:38:57 -0600

    Get signup and cancel buttons basically working

--- views/partials/form.js
+++ views/partials/form.js
@@ -2,21 +2,40 @@
   var Form = React.createClass({
     handleSubmit: function (e) {
       e.preventDefault();
-      console.log('Submitted', this.refs.name.getDOMNode().value.trim());
-      console.log('Submitted', this.refs.email.getDOMNode().value.trim());
-      console.log('Submitted', this.refs.phone.getDOMNode().value.trim());
+
+      var refs = this.refs;
+
+      console.log('Submit: ' + this.props.submit_url);
+
+      this.props.elements.forEach(function(element) {
+        var ref = element.attributes.ref;
+
+        console.log(ref + ': ' + refs[ref].getDOMNode().value.trim());
+      });
     },
     handleCancel: function (e) {
-      console.log("Cancel");
+      e.preventDefault();
+
+      var refs = this.refs;
+
+      this.props.elements.forEach(function(element) {
+        var ref = element.attributes.ref,
+            placeholder = element.attributes.placeholder;
+
+        refs[ref].getDOMNode().value = null;
+      });
     },
     render: function() {
       var elements = this.props.elements,
           children = [];
 
+      elements.forEach(function(element) {
+        children.push(React.createElement('input', element.attributes));
+        children.push(React.createElement('small', null, element.label));
+      });
+
       return React.createElement('form', null, 
-               React.createElement('input', {type: "text", placeholder:"Your name", ref:"name"}),
-               React.createElement('input', {type: "text", placeholder:"Email", ref:"email"}),
-               React.createElement('input', {type: "text", placeholder:"Phone", ref:"phone"}),
+               children,
                React.createElement('button', {onClick: this.handleCancel}, "Cancel"),
                React.createElement('button', {onClick: this.handleSubmit}, "Signup")
              );
