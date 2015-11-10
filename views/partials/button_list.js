9f690635f7c158d785dd77e688942b2aa6542b17
Benjamin Ochoa <ben@law451.com>
2015-11-09 23:19:19 -0600

    Improve styling

--- views/partials/button_list.js
+++ views/partials/button_list.js
@@ -4,8 +4,9 @@
       var buttons = this.props.buttons,
           children = [];
 
-      buttons.forEach(function(button) {
-        children.push(PJ.render_object(button.type, button, button.title));
+      buttons.forEach(function(button, i) {
+        console.log(button);
+        children.push(PJ.render_object(button.type, button, button.title + i));
       });
 
       return React.createElement('div', {className: "buttons"}, children);
