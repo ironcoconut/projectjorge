ad0837cfbc2c228bc23d607bf400a6d7dcdd2c05
Benjamin Ochoa <ben@law451.com>
2015-12-12 15:08:33 -0600

    update mailing list video, title

--- views/mailinglist.js
+++ views/mailinglist.js
@@ -5,14 +5,14 @@
             submit_function = data.submit_function,
             get_route = data.get_route,
             next_donee_slug = data.next_donee_slug,
-            summary = "Pope Francis is right. Let us use technology to end homelessness in Houston.",
+            summary = "Let's end homelessness in Houston by Christmas 2016.",
             content = "<p><a href='https://ironcoconut.github.io/projectjorge/#/donate/jorge-bergoglio' target='_blank'>View the demo.</a></p><p><a href='https://github.com/ironcoconut/projectjorge' target='_blank'>Visit project github page.</a></p>",
             message = "Thank you for joining our mailing list.";
 
         return {
           footer: View.footer("Mailinglist", next_donee_slug, get_route),
-          header: View.header("My Inspiration"),
-          media: View.media("https://www.youtube.com/embed/6WhLwWNnf3o"),
+          header: View.header("About Project Jorge"),
+          media: View.media("https://www.youtube.com/embed/wthBxLda20M"),
           info: View.info(summary, content),
           form: View.form(submit_function, message, elements)
         };
