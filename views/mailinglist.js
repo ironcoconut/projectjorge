21e4da738533e88015adbe405913c45531af57b2
Benjamin Ochoa <ben@law451.com>
2015-12-23 17:10:29 -0600

    Fix borked link

--- views/mailinglist.js
+++ views/mailinglist.js
@@ -6,7 +6,7 @@
             get_route = data.get_route,
             next_donee_slug = data.next_donee_slug,
             summary = "Let's end homelessness in Houston by Christmas 2016.",
-            content = "<p><a href='https://ironcoconut.github.io/projectjorge/#/donate/jorge-bergoglio' target='_blank'>View the demo.</a></p><p><a href='https://github.com/ironcoconut/projectjorge' target='_blank'>Visit project github page.</a></p>",
+            content = "<p><a href='https://ironcoconut.github.io/projectjorge/#/donee/jorge-bergoglio' target='_blank'>View the demo.</a></p><p><a href='https://github.com/ironcoconut/projectjorge' target='_blank'>Visit project github page.</a></p>",
             message = "Thank you for joining our mailing list.";
 
         return {
